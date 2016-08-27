0a2df55cef8fb3fdced0524db9619502bd6125cc
Jesus Alfonso Gonzalez Salinas <jagzzs@github.com>
2016-08-27 05:16:31 -0500

    Add default lat/long to all users

--- db/seeds.rb
+++ db/seeds.rb
@@ -326,10 +326,16 @@ Route.create(name: 'R-527 Alimentadora Misión San Pablo', polyline: 'i{a|ClqhbR
 Route.create(name: 'R-55 Buena Vista - Clinica 6', polyline: 'mrl|CjjkcRKzC_@`Cq@hBuAxAg`@dOgvBlx@jCtZ`@rCxCh_@OZqo@nBYRI\\jHr{@QRwl@zAUu@QwLc@MmCJiJbBeCDmHm@_FR{EdAkDRaAEeFo@{C?_BRgEz@RtJuV[m@iDo@w@kDcBmDwA`@sv@eHaByFE_Ij@iE?yC`KJhGxFjCzBtAfBgHt@{ErCSvAXdArBRzDvHmCWfi@c@|R|Qm@lQ]hx@`BV|R~o@wALIGGeo@zAU}Ruy@cBic@fAd@uRhDnA`D`Bf@f@n@pD`X`@UyJ|Cs@hBUfD?fFl@|@DhDO`FoA`FQfHn@jCEdJcB`CMTDVlMFXPNvm@uALMiHi|@JWNQno@qBZ^lGpu@fIlcARRD[wVa{CYiFuBeX^u@lx@kZnKuCdfAab@xGyCl@o@z@eAp@kBd@{DFgZToZ`@iCpAoCrCeCb\\yJH]_L{KzRqWvTcYf^_e@d@eBfKj@vBIlo@nE~BExGb@hNjAzFp@dEFb@m@vEgZpAkKAcCwC_Qlt@jRTg@Si@{t@mQgXoIcMcEmF\\}Hs@bC`_@hCd^n@dJ_@TeOk@qPcA_SsAkHYmDCg@`@Qp@s@pA}^re@eQdU{SrX~@VlAp@rKhKc\\tJmCtBgBfDe@lCUbs@')
 Route.create(name: 'R-23 Circuito Corto', polyline: 'okb|C`_ucRjZ}WfASvIjH|AlCnAtExCbB`B|CxHhFnBrD~@fDjAxA`D`C~BfFuTp[cI~GuFvJsBtCyEpI_KhNgF|FcGhD{A~AoYxb@uBxByBhDk@hBcBnH_@ZDl@XNb@KpFnArEz@dEl@`Ez@v@^v@~@M~BoFnUwBzCqBdAqGwBmJiC}RaGoSwGeMqCeC}@cIuEkMyDaPkD_GoCoCqBY{@AiArCoF`KoOvNdELVGVgCpDiDvDaClDqE`L~Ad@b@Zx@@`JdBvNjElHvEdUvFzFdCtGnBp\\nIzGjCx@Tl@@bCiBdBqCvFsVTuDiAc@k@AcI_BkHkAeGwAUs@zAoHr@kBnBuCbCmCdYgb@jAkAnDmB`BkA`FyFdKsNNc@Ii@}DuHc@_BeDuD}Aq@gBO{Dj@gB]iBiAcScQcPsUGQFIxc@}`@')
 
+latitude = 25.670312
+longitude = -100.3772177217
+
 route_400 = Route.find_by(name: 'R-400 Fleteros')
-route_400.drivers << Driver.new(name: "Juan", email: "juan_400@driver.com", password: "testingpassword", password_confirmation: "testingpassword", role: "Driver")
-route_400.drivers << Driver.new(name: "Luis", email: "luis_400@driver.com", password: "testingpassword", password_confirmation: "testingpassword", role: "Driver")
+route_400.drivers << Driver.new(name: "Juan", email: "juan_400@driver.com", password: "testingpassword", password_confirmation: "testingpassword", role: "Driver", latitude: latitude, longitude: longitude)
+route_400.drivers << Driver.new(name: "Luis", email: "luis_400@driver.com", password: "testingpassword", password_confirmation: "testingpassword", role: "Driver", latitude: latitude, longitude: longitude)
 
 route_214 = Route.find_by(name: 'R-214 Guadalupe Directo')
-route_214.drivers << Driver.new(name: "Jose", email: "jose_214@driver.com", password: "testingpassword", password_confirmation: "testingpassword", role: "Driver")
-route_214.drivers << Driver.new(name: "Hector", email: "hector_214@driver.com", password: "testingpassword", password_confirmation: "testingpassword", role: "Driver")
+route_214.drivers << Driver.new(name: "Jose", email: "jose_214@driver.com", password: "testingpassword", password_confirmation: "testingpassword", role: "Driver", latitude: latitude, longitude: longitude)
+route_214.drivers << Driver.new(name: "Hector", email: "hector_214@driver.com", password: "testingpassword", password_confirmation: "testingpassword", role: "Driver", latitude: latitude, longitude: longitude)
+
+Client.create(name: "Alfredo", email: "alfredo@test.com", password: "testpass", password_confirmation: "testpass", role: "Client")
+Admin.create(name: "Admin", email: "admin@test.com", password: "testpass", password_confirmation: "testpass", role: "Admin")
