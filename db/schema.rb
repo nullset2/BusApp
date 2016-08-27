af93fd134146684403d09ad8c9035cadf4a506ae
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-26 23:31:07 -0500

    fixes db fuckups

--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,10 @@ ActiveRecord::Schema.define(version: 20160827031744) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
@@ -80,6 +84,7 @@ ActiveRecord::Schema.define(version: 20160827031744) do
     t.json     "tokens"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
