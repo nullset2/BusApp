f3cc0f19b8c0226609c6b91ae04ee621370e51fe
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-26 23:30:47 -0500

    temp disable emails on local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
