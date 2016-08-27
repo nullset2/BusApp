1624b7889c2825a1090548e60a8708a7f8aa6345
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-26 23:23:18 -0500

    adds validations DUH

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,18 @@ class User < ActiveRecord::Base
   inheritance_column = :role
   after_initialize :set_default_role, :if => :new_record?
 
-  def set_default_role
-    self.role ||= "User"
+  def set_default_role(role)
+    self.role ||= "Client"
   end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def confirmed_at
-  end
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: Devise.email_regexp
+  validates :role, presence: true
 
-  def confirmation_token 
-  end
 end
