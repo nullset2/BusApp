6e087963717fea6775983c5eab30c9acc69b37de
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-27 01:50:50 -0500

    better session and csrf management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   protected
   
