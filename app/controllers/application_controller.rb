785dcccf45367dbb125d8ac7198011f5d41cb2e1
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-26 20:04:59 -0500

    adds parameter sanitization for user acct

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_filter configure_permitted_parameters, if: devise_controller?
   protect_from_forgery with: :exception
+
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :latitude, :longitude, :role])
+  end
 end
