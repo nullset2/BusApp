29ee348c3db8a28b501752612298d0592fdf2d59
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-26 22:50:48 -0500

    lolnope

--- db/migrate/20160827034817_add_missing_fields_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddMissingFieldsToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :confirmation_token, :string
-    add_column :users, :confirmed_at, :datetime
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827034817) do
+ActiveRecord::Schema.define(version: 20160827031744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,8 +78,6 @@ ActiveRecord::Schema.define(version: 20160827034817) do
     t.string   "provider",               default: "email", null: false
     t.string   "uid",                    default: "",      null: false
     t.json     "tokens"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
