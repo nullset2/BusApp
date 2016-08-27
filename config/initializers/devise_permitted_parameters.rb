98a664c7da2695f74097a79dfb9fe978e6d7e9b5
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-27 09:47:08 -0500

    better permitted parameter management

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,8 +8,8 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :latitude, :longitude, :role])
   end
 
 end
