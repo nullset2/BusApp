1bbb2085156cb4f584e1650e9eb8412240891da6
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-27 03:58:54 -0500

    better management on controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -3,6 +3,12 @@ class DriversController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    render(json: { errors: "Submit all required params" }, status: :unprocessable_entity) and return if params[:search_distance].blank?
     @drivers = Driver.within_bounding_box(get_bounding_box(current_user, params[:search_distance].to_f))
+    if !@drivers.empty?
+      return @drivers
+    else
+      render json: { errors: "No drivers found" }, status: :not_found
+    end
   end
 end
