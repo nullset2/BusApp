f8cb5b458a50663cd4faee45f3e929740c8c92fa
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-27 01:05:45 -0500

    adds missing resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     mount_devise_token_auth_for 'User', at: 'auth'
   end
 
+  resources :users, :routes, :favorites, :stop_requests
   root to: 'visitors#index'
-
-  resources :routes
 end
