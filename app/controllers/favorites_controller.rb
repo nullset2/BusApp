6c87e5908734cb208f12db1d461dc0b74b89832f
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-27 08:24:12 -0500

    adds favorites controller

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,66 @@
+class FavoritesController < ApplicationController
+  before_action :authenticate_use!
+  before_action :set_favorite, only: [:show, :edit, :update, :destroy]
+
+  # GET /favorites
+  # GET /favorites.json
+  def index
+    @favorites = current_user.favorites || render json: { errors: "No favorites for current user" }, status: :not_found
+  end
+
+  # GET /favorites/1
+  # GET /favorites/1.json
+  def show
+  end
+
+  # POST /favorites
+  # POST /favorites.json
+  def create
+    @favorite = current_user.favorites << Favorite.new(favorite_params)
+
+    respond_to do |format|
+      if @favorite.save
+        format.html { redirect_to @favorite, notice: 'Favorite was successfully created.' }
+        format.json { render :show, status: :created, location: @favorite }
+      else
+        format.html { render :new }
+        format.json { render json: @favorite.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /favorites/1
+  # PATCH/PUT /favorites/1.json
+  def update
+    respond_to do |format|
+      if @favorite.update(favorite_params)
+        format.html { redirect_to @favorite, notice: 'Favorite was successfully updated.' }
+        format.json { render :show, status: :ok, location: @favorite }
+      else
+        format.html { render :edit }
+        format.json { render json: @favorite.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /favorites/1
+  # DELETE /favorites/1.json
+  def destroy
+    @favorite.destroy
+    respond_to do |format|
+      format.html { redirect_to favorites_url, notice: 'Favorite was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_favorite
+      @favorite = Favorite.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def favorite_params
+      params[:favorite]
+    end
+end
