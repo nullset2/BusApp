0ed9b603d5d2ea70f7955708f7cac6255d93bbdd
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-27 09:42:13 -0500

    switches param validation from application_controller to initializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :null_session
-
-  protected
-  
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :latitude, :longitude, :role])
-  end
 end
